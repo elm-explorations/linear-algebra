b2e3faa50bdf557c67cd87d6a2b7b3eeb353f7d8
Robert Wallis <smilingrob@gmail.com>
2018-03-09 20:35:31 -0800

    Updating to elm-test 4.0.0 because following the steps in .travis.yml installing elm-test will use version 4, and version 3 does not run anymore.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,12 +1,12 @@
 module Tests exposing (suite)
 
-import Test exposing (Test, describe, test, fuzz, fuzz3)
-import Fuzz
 import Expect
+import Fuzz
+import Math.Matrix4 as M4
 import Math.Vector2 as V2
 import Math.Vector3 as V3
 import Math.Vector4 as V4
-import Math.Matrix4 as M4
+import Test exposing (Test, describe, fuzz, fuzz3, test)
 
 
 suite : Test
@@ -73,7 +73,8 @@ suite =
                             |> Expect.equal (Just (M4.makeTranslate3 -x -y -z))
                 , Test.concat <|
                     -- using constants instead of fuzz because of floating point errors
-                    (flip List.map) [ pi, 1.5 * pi, pi / 12, pi / 50 ]
+                    flip List.map
+                        [ pi, 1.5 * pi, pi / 12, pi / 50 ]
                         (\theta ->
                             test ("Rotation by " ++ toString theta) <|
                                 \_ ->
@@ -81,10 +82,10 @@ suite =
                                         m =
                                             M4.makeRotate theta V3.i
                                     in
-                                        m
-                                            |> M4.inverse
-                                            |> Maybe.map (M4.mul m)
-                                            |> Expect.equal (Just M4.identity)
+                                    m
+                                        |> M4.inverse
+                                        |> Maybe.map (M4.mul m)
+                                        |> Expect.equal (Just M4.identity)
                         )
                 , test "cannot invert a singular matrix" <|
                     \_ ->
@@ -108,13 +109,13 @@ suite =
                                 )
                 , test "makeFromList should be able create identity matrix" <|
                     \_ ->
-                        M4.makeFromList ([ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ]) |> Expect.equal (Just M4.identity)
+                        M4.makeFromList [ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ] |> Expect.equal (Just M4.identity)
                 , test "makeFromList should return Nothing for short list of elements" <|
                     \_ ->
-                        M4.makeFromList ([]) |> Expect.equal Nothing
+                        M4.makeFromList [] |> Expect.equal Nothing
                 , test "makeFromList should return Nothing for too long list of elements" <|
                     \_ ->
-                        M4.makeFromList ([ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1 ]) |> Expect.equal Nothing
+                        M4.makeFromList [ 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1 ] |> Expect.equal Nothing
                 , test "fromRecord should be able to create the identity matrix" <|
                     \_ ->
                         M4.fromRecord { m11 = 1, m21 = 0, m31 = 0, m41 = 0, m12 = 0, m22 = 1, m32 = 0, m42 = 0, m13 = 0, m23 = 0, m33 = 1, m43 = 0, m14 = 0, m24 = 0, m34 = 0, m44 = 1 } |> Expect.equal M4.identity
@@ -124,17 +125,17 @@ suite =
                 , fuzz fuzz4x4 "fromRecord should be the opposite of toRecord" <|
                     \record4x4 ->
                         M4.toRecord (M4.fromRecord record4x4) |> Expect.equal record4x4
-                , test "matrix should not lose precision" <|
+                , test "matrix from list should not lose precision" <|
                     \_ ->
                         let
                             matrix =
                                 M4.makeFromList
-                                    ([ 0.0007645259938837921, 0, 0, 0, 0, 0.0007645259938837921, 0, 0, -178844.39532879056, -115444.72328862673, -23930.400791413915, -0.4996215217852649, 179114.76199136386, 115619.29560235815, 23930.038231557068, 0.5003784782148529 ])
+                                    [ 0.0007645259938837921, 0, 0, 0, 0, 0.0007645259938837921, 0, 0, -178844.39532879056, -115444.72328862673, -23930.400791413915, -0.4996215217852649, 179114.76199136386, 115619.29560235815, 23930.038231557068, 0.5003784782148529 ]
                         in
-                            matrix
-                                |> toString
-                                |> Expect.equal "Just { 0 = 0.0007645259938837921, 1 = 0, 2 = 0, 3 = 0, 4 = 0, 5 = 0.0007645259938837921, 6 = 0, 7 = 0, 8 = -178844.39532879056, 9 = -115444.72328862673, 10 = -23930.400791413915, 11 = -0.4996215217852649, 12 = 179114.76199136386, 13 = 115619.29560235815, 14 = 23930.038231557068, 15 = 0.5003784782148529 }"
-                , test "matrix should not lose precision" <|
+                        matrix
+                            |> toString
+                            |> Expect.equal "Just { 0 = 0.0007645259938837921, 1 = 0, 2 = 0, 3 = 0, 4 = 0, 5 = 0.0007645259938837921, 6 = 0, 7 = 0, 8 = -178844.39532879056, 9 = -115444.72328862673, 10 = -23930.400791413915, 11 = -0.4996215217852649, 12 = 179114.76199136386, 13 = 115619.29560235815, 14 = 23930.038231557068, 15 = 0.5003784782148529 }"
+                , test "matrix make basis should not lose precision" <|
                     \_ ->
                         let
                             matrix =
@@ -143,9 +144,9 @@ suite =
                                     (V3.fromTuple ( 0, 0, 0 ))
                                     (V3.fromTuple ( 0, 0, -178844.39532879056 ))
                         in
-                            matrix
-                                |> toString
-                                |> Expect.equal "{ 0 = 0.0007645259938837921, 1 = 0, 2 = 0, 3 = 0, 4 = 0, 5 = 0, 6 = 0, 7 = 0, 8 = 0, 9 = 0, 10 = -178844.39532879056, 11 = 0, 12 = 0, 13 = 0, 14 = 0, 15 = 1 }"
+                        matrix
+                            |> toString
+                            |> Expect.equal "{ 0 = 0.0007645259938837921, 1 = 0, 2 = 0, 3 = 0, 4 = 0, 5 = 0, 6 = 0, 7 = 0, 8 = 0, 9 = 0, 10 = -178844.39532879056, 11 = 0, 12 = 0, 13 = 0, 14 = 0, 15 = 1 }"
                 ]
             ]
         ]
